60bf78a1d7f96be571c822f3f14f342277bea89d
Anh Tran <anhtnt@elightup.com>
2020-04-07 11:48:35 +0700

    Update style for recent posts widget

--- src/Widgets/RecentPosts.php
+++ src/Widgets/RecentPosts.php
@@ -5,12 +5,18 @@ use WP_Widget;
 use WP_Query;
 
 class RecentPosts extends WP_Widget {
+	private $defaults;
+
 	public function __construct() {
-		$widget_ops = array(
-			'classname'                   => 'erocket-recent-posts',
-			'customize_selective_refresh' => true,
-		);
-		parent::__construct( 'erocket-recent-posts', __( '[eRocket] Recent Posts', 'erocket' ), $widget_ops );
+		$this->defaults = [
+			'title'    => __( 'Recent Posts', 'erocket' ),
+			'number'   => 5,
+			'category' => '',
+		];
+
+		parent::__construct( 'erp', __( '[eRocket] Recent Posts', 'erocket' ), [
+			'classname' => 'erp',
+		] );
 
 		if ( is_active_widget( false, false, $this->id_base ) || is_customize_preview() ) {
 			add_action( 'wp_head', [ $this, 'output_style' ] );
@@ -20,30 +26,30 @@ class RecentPosts extends WP_Widget {
 	public function output_style() {
 		?>
 		<style>
-		.recent-post {
+		.erp li {
 			display: flex;
 		}
-		.recent-post:not(:last-child) {
+		.erp li:not(:last-child) {
 			margin-bottom: 16px;
 		}
-		.recent-post-thumbnail {
+		.erp li > a {
 			display: block;
 			margin-right: 12px;
 		}
-		.recent-post img {
+		.erp img {
 			display: block;
 			width: 64px;
 			height: 64px;
 		}
-		.recent-post-body {
+		.erp-body {
 			flex: 1;
 			line-height: 1.25;
 		}
-		.recent-post-body a {
+		.erp a {
 			font-weight: 700;
 			color: inherit;
 		}
-		.recent-post-body time {
+		.erp time {
 			display: block;
 			margin-top: 4px;
 			color: #a0aec0;
@@ -54,42 +60,41 @@ class RecentPosts extends WP_Widget {
 	}
 
 	public function widget( $args, $instance ) {
-		$title = empty( $instance['title'] ) ? __( 'Recent Posts', 'erocket' ) : $instance['title'];
-		$title = apply_filters( 'widget_title', $title, $instance, $this->id_base );
+		$instance = wp_parse_args( $instance, $this->defaults );
 
-		$number = empty( $instance['number'] ) ? 5 : absint( $instance['number'] );
-		if ( ! $number ) {
-			$number = 5;
-		}
-
-		$query = new WP_Query( [
-			'posts_per_page'         => $number,
+		$query_args = [
+			'posts_per_page'         => $instance['number'],
 			'no_found_rows'          => true,
 			'post_status'            => 'publish',
 			'ignore_sticky_posts'    => true,
 			'update_post_meta_cache' => false,
 			'update_post_term_cache' => false,
-		] );
+		];
+		if ( ! empty( $instance['category'] ) ) {
+			$query_args['cat'] = $instance['category'];
+		}
 
+		$query = new WP_Query( $query_args );
 		if ( ! $query->have_posts() ) {
 			return;
 		}
 
 		echo $args['before_widget'];
 
+		$title = apply_filters( 'widget_title', $instance['title'], $instance, $this->id_base );
 		if ( $title ) {
 			echo $args['before_title'] . $title . $args['after_title'];
 		}
 		?>
-		<ul class="recent-posts">
+		<ul>
 			<?php while ( $query->have_posts() ) : $query->the_post(); ?>
-				<li class="recent-post">
+				<li>
 					<?php if ( has_post_thumbnail() ) : ?>
-						<a class="recent-post-thumbnail" href="<?php the_permalink(); ?>">
+						<a href="<?php the_permalink(); ?>">
 							<?php the_post_thumbnail( 'thumbnail' ); ?>
 						</a>
 					<?php endif; ?>
-					<div class="recent-post-body">
+					<div class="erp-body">
 						<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 						<time><?php echo esc_html( get_the_date() ); ?></time>
 					</div>
@@ -103,21 +108,39 @@ class RecentPosts extends WP_Widget {
 	}
 
 	public function update( $new_instance, $old_instance ) {
-		$instance           = $old_instance;
-		$instance['title']  = sanitize_text_field( $new_instance['title'] );
-		$instance['number'] = (int) $new_instance['number'];
+		$instance             = $old_instance;
+		$instance['title']    = sanitize_text_field( $new_instance['title'] );
+		$instance['number']   = absint( $new_instance['number'] );
+		$instance['category'] = absint( $new_instance['category'] );
 		return $instance;
 	}
 
 	public function form( $instance ) {
-		$title  = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
-		$number = isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
+		$instance = wp_parse_args( $instance, $this->defaults );
 		?>
-		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e( 'Title:', 'erocket' ); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>"></p>
-
-		<p><label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php esc_html_e( 'Number of posts to show:', 'erocket' ); ?></label>
-		<input class="tiny-text" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="number" step="1" min="1" value="<?php echo esc_attr( $number ); ?>" size="3"></p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e( 'Title:', 'erocket' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $instance['title'] ); ?>">
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'category' ); ?>"><?php esc_html_e( 'Category:', 'erocket' ); ?></label>
+			<?php
+			wp_dropdown_categories( [
+				'show_option_all' => __( 'All', 'erocket' ),
+				'orderby'         => 'name',
+				'order'           => 'ASC',
+				'selected'        => $instance['category'],
+				'hierarchical'    => true,
+				'name'            => $this->get_field_name( 'category' ),
+				'id'              => $this->get_field_id( 'category' ),
+				'class'           => 'widefat',
+			] );
+			?>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php esc_html_e( 'Number of posts to show:', 'erocket' ); ?></label>
+			<input class="tiny-text" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="number" step="1" min="1" value="<?php echo esc_attr( $instance['number'] ); ?>" size="3">
+		</p>
 		<?php
 	}
 }
