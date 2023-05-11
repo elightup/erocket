0a01589f32b6852acb06611fc73e37524829376d
Anh Tran <anhtnt@elightup.com>
2023-05-11 11:25:05 +0700

    Version 1.2.5

--- erocket.php
+++ erocket.php
@@ -1,15 +1,15 @@
 <?php
 /**
  * Plugin Name: eRocket
- * Plugin URI:  https://elightup.com/products/
+ * Plugin URI:  https://elightup.com
  * Description: Superchange your WordPress websites with premium features.
  * Author:      eLightUp
  * Author URI:  https://elightup.com
  * Text Domain: erocket
  * Domain Path: /languages
- * Version:     1.2.4
+ * Version:     1.2.5
  */
 
 require __DIR__ . '/vendor/autoload.php';
 
-new ERocket\Loader;
+new ERocket\Loader();
